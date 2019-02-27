beeceb5b89ea4bb6128248764bb328beb6db06dd
AAM77 <Adeel.A.Mhd@gmail.com>
2019-02-27 14:56:40 -0500

    Updated

---
+++ round 1 code/r1d21_files/cap.py
@@ -0,0 +1,7 @@
+def cap_text(test):
+    '''
+    Input a string
+    Output the capitalized string
+    '''
+
+    return test.title()
---
+++ round 1 code/r1d21_files/simple1.py
@@ -0,0 +1,14 @@
+'''
+A VERY SIMPLE SCRIPT
+'''
+
+def myfunc():
+    '''
+    A simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
